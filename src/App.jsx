73644abdc4ae86d1a14aadf8982de533d0976926
muddassirk <mkkhanu72@gmail.com>
2021-08-07 03:32:44 +0500

    search icon

--- src/App.jsx
+++ src/App.jsx
@@ -21,17 +21,35 @@ function App() {
           Weather App
         </h1>
         <div className="container-fluid a">
-          <label forhtml="cityName" className="form-label">Enter city name</label>
-          <input className="form-control" list="datalistOptions" id="cityName" placeholder="Enter city name..." />
-          <datalist id="datalistOptions">
-            <option value="Karachi" />
-            <option value="Islamabad" />
-            <option value="San Francisco" />
-            <option value="New York" />
-            <option value="Seattle" />
-            <option value="Los Angeles" />
-            <option value="Chicago" />
-          </datalist>
+          <form >
+            <div className="input-group my-3">
+              {/*         <input
+              type="text"
+              className="form-control"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            /> */}
+              {/* <label forhtml="cityName" className="form-label">Enter city name</label> */}
+              <input className="form-control" list="datalistOptions" id="cityName" placeholder="Enter city name..." />
+              {/* <button className="btn btn-outline-Primary" type="button" id="button-addon2">Button</button> */}
+              <button type="submit" className="btn btn-outline-primary" style={{ border: "1px solid white" }} >
+                <i className="fa fa-search" style={{ color: 'white' }}></i>
+              </button>
+
+              <datalist id="datalistOptions">
+                <option value="Karachi" />
+                <option value="Islamabad" />
+                <option value="San Francisco" />
+                <option value="New York" />
+                <option value="Seattle" />
+                <option value="Los Angeles" />
+                <option value="Chicago" />
+              </datalist>
+            </div>
+          </form>
+
+
           <div className="w-100 py-4">
             <h1>
               Karachi, PK
@@ -46,7 +64,7 @@ function App() {
             <div className="col-sm-6">
               <h1>21 degree</h1>
             </div>
-            
+
             <div className="col-sm-6">
               <div className="col">
                 <div className="row">
